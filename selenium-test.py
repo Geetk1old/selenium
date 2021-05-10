15bec2ae8e675b9c1e0d9ac6d772c90bfbf5ca68
Geetk1old <1326052552@qq.com>
2021-05-10 17:11:16 +0800

    12

--- selenium-test.py
+++ selenium-test.py
@@ -11,10 +11,10 @@ chrome_driver_binary="C:/Program Files/Google/Chrome/Application/chromedriver.ex
 driver=webdriver.Chrome(chrome_driver_binary, chrome_options=options)
 #测试的网页，打开网页
 driver.get("http://www.baidu.com")
-sleep(2)
+sleep(3)
 #找到id为“kw”的控件，输入测试文本Test search
 driver.find_element_by_id("kw").send_keys("Test search")
 #找到id为“su”的控件，模拟鼠标点击
 driver.find_element_by_id("su").click()
-sleep(2)
+sleep(3)
 driver.quit()
